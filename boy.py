73196fb9c024a24fd821a777956d78f9059c40be
qkd0863 <qkd7183@naver.com>
2023-10-12 21:39:03 +0900

    Drill 09 제출 (2018180022 방영규)

--- boy.py
+++ boy.py
@@ -2,7 +2,7 @@
 import math
 
 from pico2d import load_image, get_time
-from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK_UP
 
 
 def right_down(e):
@@ -21,12 +21,55 @@ def left_up(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
 
 
+def auto_run(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == 97
+
+
 def space_down(e):
     return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
 
 
 def time_out(e):
     return e[0] == 'TIME_OUT'
+
+
+class AutoRun:
+    @staticmethod
+    def enter(boy, e):
+        if boy.dir == 0:
+            boy.dir, boy.action = 1, 1
+        elif boy.dir == -1:
+            boy.dir, boy.action = -1, 0
+        boy.start_time = get_time()
+        print('AutoRun Enter')
+
+    @staticmethod
+    def exit(boy, e):
+        print('AutoRun Exit')
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        print(boy.dir)
+        if boy.x <= 700 and boy.dir == 1:
+            boy.x += boy.dir * 10
+        elif boy.x >= 0 and boy.dir == -1:
+            boy.x += boy.dir * 10
+        if boy.x > 700:
+            boy.dir = -1
+            boy.action = 0
+        if boy.x < 0:
+            boy.dir = 1
+            boy.action = 1
+
+        if get_time() - boy.start_time > 5:
+            boy.state_machine.handle_event(('TIME_OUT', 0))
+        pass
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x + 25, boy.y + 25, 200, 200)
 
 
 class Run:
@@ -36,9 +79,11 @@ class Run:
             boy.dir, boy.action = 1, 1
         elif left_down(e) or right_up(e):
             boy.dir, boy.action = -1, 0
+        print('Run Enter')
 
     @staticmethod
     def exit(boy, e):
+        print('Run Exit')
         pass
 
     @staticmethod
@@ -49,7 +94,7 @@ class Run:
 
     @staticmethod
     def draw(boy):
-        boy.image.clip_draw(boy.frame * 100, boy.action, 100, 100, boy.x, boy.y)
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
 
 
 class Idle:
@@ -72,8 +117,8 @@ class Idle:
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + 1) % 8
-        print('Idle Do')
-        if get_time() - boy.start_time > 3.0:
+
+        if get_time() - boy.start_time > 3:
             boy.state_machine.handle_event(('TIME_OUT', 0))
 
     @staticmethod
@@ -96,12 +141,11 @@ class Sleep:
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + 1) % 8
-        print('드르렁')
 
     @staticmethod
     def draw(boy):
         if boy.action == 2:
-            boy.image.clip_composite_draw(boy.frame * 100, 200, 100, 100, math.pi / 2 * -1, '', boy.x - 25, boy.y - 25,
+            boy.image.clip_composite_draw(boy.frame * 100, 200, 100, 100, math.pi / 2 * -1, '', boy.x + 25, boy.y - 25,
                                           100, 100)
         else:
             boy.image.clip_composite_draw(boy.frame * 100, 300, 100, 100, math.pi / 2, '', boy.x - 25, boy.y - 25, 100,
@@ -114,9 +158,10 @@ class StateMachine:
         self.boy = boy
         self.cur_state = Idle
         self.table = {
+            Idle: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, time_out: Sleep, auto_run: AutoRun},
+            Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle},
             Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle},
-            Idle: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, time_out: Sleep},
-            Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle}
+            AutoRun: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, time_out: Idle}
         }
 
     def start(self):
@@ -132,8 +177,7 @@ class StateMachine:
                 self.cur_state = next_state
                 self.cur_state.enter(self.boy, e)
                 return True  # 성공적으로 이벤트 변환
-            return False  # 이벤트를 소모하지 못함
-        pass
+        return False  # 이벤트를 소모하지 못함
 
     def draw(self):
         self.cur_state.draw(self.boy)
