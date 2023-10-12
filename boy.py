66a173a9c6070ebcf92f01549c48c1cf88eb675e
qkd0863 <58385389+qkd0863@users.noreply.github.com>
2023-10-12 13:13:25 +0900

    123

--- boy.py
+++ boy.py
@@ -1,24 +1,80 @@
 # 이것은 각 상태들을 객체로 구현한 것임.
 import math
 
-from pico2d import load_image
+from pico2d import load_image, get_time
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT
+
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+
+
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+
+
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+
+
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
+
+def space_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+
+
+def time_out(e):
+    return e[0] == 'TIME_OUT'
+
+
+class Run:
+    @staticmethod
+    def enter(boy, e):
+        if right_down(e) or left_up(e):
+            boy.dir, boy.action = 1, 1
+        elif left_down(e) or right_up(e):
+            boy.dir, boy.action = -1, 0
+
+    @staticmethod
+    def exit(boy, e):
+        pass
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        boy.x += boy.dir * 5
+        pass
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action, 100, 100, boy.x, boy.y)
 
 
 class Idle:
 
     @staticmethod
-    def enter(boy):
+    def enter(boy, e):
+        if boy.action == 0:
+            boy.action = 2
+        elif boy.action == 1:
+            boy.action = 3
+        boy.dir = 0
         boy.frame = 0
+        boy.start_time = get_time()
         print('Idle Enter')
 
     @staticmethod
-    def exit(boy):
+    def exit(boy, e):
         print('Idle Exit')
 
     @staticmethod
     def do(boy):
         boy.frame = (boy.frame + 1) % 8
         print('Idle Do')
+        if get_time() - boy.start_time > 3.0:
+            boy.state_machine.handle_event(('TIME_OUT', 0))
 
     @staticmethod
     def draw(boy):
@@ -29,12 +85,12 @@ class Idle:
 class Sleep:
 
     @staticmethod
-    def enter(boy):
+    def enter(boy, e):
         boy.frame = 0
         print('고개 숙이기')
 
     @staticmethod
-    def exit(boy):
+    def exit(boy, e):
         print('고개 들기')
 
     @staticmethod
@@ -44,20 +100,40 @@ class Sleep:
 
     @staticmethod
     def draw(boy):
-        boy.image.clip_composite_draw(boy.frame * 100, 300, 100, 100, math.pi / 2, '', boy.x - 25, boy.y - 25, 100, 100)
+        if boy.action == 2:
+            boy.image.clip_composite_draw(boy.frame * 100, 200, 100, 100, math.pi / 2 * -1, '', boy.x - 25, boy.y - 25,
+                                          100, 100)
+        else:
+            boy.image.clip_composite_draw(boy.frame * 100, 300, 100, 100, math.pi / 2, '', boy.x - 25, boy.y - 25, 100,
+                                          100)
         pass
 
 
 class StateMachine:
     def __init__(self, boy):
         self.boy = boy
-        self.cur_state = Sleep
+        self.cur_state = Idle
+        self.table = {
+            Sleep: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, space_down: Idle},
+            Idle: {right_down: Run, left_down: Run, right_up: Run, left_up: Run, time_out: Sleep},
+            Run: {right_down: Idle, left_down: Idle, right_up: Idle, left_up: Idle}
+        }
 
     def start(self):
-        self.cur_state.enter(self.boy)
+        self.cur_state.enter(self.boy, ('START', 0))
 
     def update(self):
         self.cur_state.do(self.boy)
+
+    def handle_event(self, e):
+        for check_event, next_state in self.table[self.cur_state].items():
+            if check_event(e):
+                self.cur_state.exit(self.boy, e)
+                self.cur_state = next_state
+                self.cur_state.enter(self.boy, e)
+                return True  # 성공적으로 이벤트 변환
+            return False  # 이벤트를 소모하지 못함
+        pass
 
     def draw(self):
         self.cur_state.draw(self.boy)
@@ -76,6 +152,7 @@ class Boy:
         self.state_machine.update()
 
     def handle_event(self, event):
+        self.state_machine.handle_event(('INPUT', event))
         pass
 
     def draw(self):
---
+++ simple.py
@@ -0,0 +1,3 @@
+data = {1: 'Kim', 3: 'Park', 4: 'Lee'}
+for no, name in data.items():
+    print(no, name)
