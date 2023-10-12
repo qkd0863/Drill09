51642c11f9bd22a130c1f25133b839b0b86bc4af
qkd0863 <58385389+qkd0863@users.noreply.github.com>
2023-10-12 12:05:02 +0900

    준비

---
+++ boy.py
@@ -0,0 +1,82 @@
+# 이것은 각 상태들을 객체로 구현한 것임.
+import math
+
+from pico2d import load_image
+
+
+class Idle:
+
+    @staticmethod
+    def enter(boy):
+        boy.frame = 0
+        print('Idle Enter')
+
+    @staticmethod
+    def exit(boy):
+        print('Idle Exit')
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        print('Idle Do')
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+        pass
+
+
+class Sleep:
+
+    @staticmethod
+    def enter(boy):
+        boy.frame = 0
+        print('고개 숙이기')
+
+    @staticmethod
+    def exit(boy):
+        print('고개 들기')
+
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+        print('드르렁')
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_composite_draw(boy.frame * 100, 300, 100, 100, math.pi / 2, '', boy.x - 25, boy.y - 25, 100, 100)
+        pass
+
+
+class StateMachine:
+    def __init__(self, boy):
+        self.boy = boy
+        self.cur_state = Sleep
+
+    def start(self):
+        self.cur_state.enter(self.boy)
+
+    def update(self):
+        self.cur_state.do(self.boy)
+
+    def draw(self):
+        self.cur_state.draw(self.boy)
+
+
+class Boy:
+    def __init__(self):
+        self.x, self.y = 400, 90
+        self.frame = 0
+        self.action = 3
+        self.image = load_image('animation_sheet.png')
+        self.state_machine = StateMachine(self)
+        self.state_machine.start()
+
+    def update(self):
+        self.state_machine.update()
+
+    def handle_event(self, event):
+        pass
+
+    def draw(self):
+        self.state_machine.draw()
